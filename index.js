25439a1b9b9483518226e3582238722b49e502fc
srinivasa ranganath <91521184+flawless933@users.noreply.github.com>
2023-02-07 01:47:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,38 @@
-const download = document.querySelector(".download");
-const dark = document.querySelector(".dark");
-const light = document.querySelector(".light");
-const qrContainer = document.querySelector("#qr-code");
-const qrText = document.querySelector(".qr-text");
-const shareBtn = document.querySelector(".share-btn");
-const sizes = document.querySelector(".sizes");
+const wrapper = document.querySelector(".wrapper"),
+form = document.querySelector("form"),
+fileInp = form.querySelector("input"),
+infoText = form.querySelector("p"),
+closeBtn = document.querySelector(".close"),
+copyBtn = document.querySelector(".copy");
 
-dark.addEventListener("input", handleDarkColor);
-light.addEventListener("input", handleLightColor);
-qrText.addEventListener("input", handleQRText);
-sizes.addEventListener("change", handleSize);
-shareBtn.addEventListener("click", handleShare);
-
-
-let colorLight = "#fff",
-    colorDark = "#000",
-    text = defaultUrl,
-    size = 300;
-
-function handleDarkColor(e) {
-    colorDark = e.target.value;
-    generateQRCode();
-}
-
-function handleLightColor(e) {
-    colorLight = e.target.value;
-    generateQRCode();
-}
-
-function handleQRText(e) {
-    const value = e.target.value;
-    text = value;
-    if (!value) {
-        text = defaultUrl;
-    }
-    generateQRCode();
-}
-
-async function generateQRCode() {
-    qrContainer.innerHTML = "";
-    new QRCode("qr-code", {
-        text,
-        height: size,
-        width: size,
-        colorLight,
-        colorDark,
-    });
-    download.href = await resolveDataUrl();
-}
-
-async function handleShare() {
-    setTimeout(async () => {
-        try {
-            const base64url = await resolveDataUrl();
-            const blob = await (await fetch(base64url)).blob();
-            const file = new File([blob], "QRCode.png", {
-                type: blob.type,
-            });
-            await navigator.share({
-                files: [file],
-                title: text,
-            });
-        } catch (error) {
-            alert("Your browser doesn't support sharing.");
-        }
-    }, 100);
-}
-
-function handleSize(e) {
-    size = e.target.value;
-    generateQRCode();
-}
-
-function resolveDataUrl() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const img = document.querySelector("#qr-code img");
-            if (img.currentSrc) {
-                resolve(img.currentSrc);
-                return;
-            }
-            const canvas = document.querySelector("canvas");
-            resolve(canvas.toDataURL());
-        }, 50);
+function fetchRequest(file, formData) {
+    infoText.innerText = "Scanning QR Code...";
+    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+        method: 'POST', body: formData
+    }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
+        if(!result) return;
+        document.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
+        wrapper.classList.add("active");
+    }).catch(() => {
+        infoText.innerText = "Couldn't scan QR Code";
     });
 }
-generateQRCode();
+
+fileInp.addEventListener("change", async e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchRequest(file, formData);
+});
+
+copyBtn.addEventListener("click", () => {
+    let text = document.querySelector("textarea").textContent;
+    navigator.clipboard.writeText(text);
+});
+
+form.addEventListener("click", () => fileInp.click());
+closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
